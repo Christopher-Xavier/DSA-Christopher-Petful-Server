d2d4f5f4420ea861f830f50eccbc6314f2d6c43a
Christopher-Bogan <60634997+Christopher-Xavier@users.noreply.github.com>
2020-10-06 15:16:26 -0400

    Delete server.js

--- src/server.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable strict */
-
-const app = require('./modules/app/app');
-const port = process.env.PORT || 8080;
-
-app.listen(3000, () => {
-  console.log(`[petful-server] Listening on ${ port }.`);
-});
