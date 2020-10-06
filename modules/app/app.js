7089ba7748e2fa12d0afe4312acfc6767daf99b3
Christopher-Bogan <60634997+Christopher-Xavier@users.noreply.github.com>
2020-10-06 15:01:06 -0400

    Update app.js

--- modules/app/app.js
+++ modules/app/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const PeopleRouter = require('../people/people.router');
 const PetsRouter = require('../pets/pets.router');
 
-const CLIENT_ORIGIN = 'https://dsa-petful-client-james.vercel.app' || 'http://localhost:3000';
+const CLIENT_ORIGIN = 'https://petful-client-blue.vercel.app/' || 'http://localhost:3000';
 
 const app = express();
 
@@ -13,4 +13,7 @@ app.use(cors({origin: CLIENT_ORIGIN}));
 app.use('/api/people', PeopleRouter);
 app.use('/api/pets', PetsRouter);
 
+app.get('/hello', (req, res) => {
+res.status(200).send('Hello')
+})
 module.exports = app;
